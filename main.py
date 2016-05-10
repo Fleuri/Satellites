3300e38a3d2e0c1baf2557f25dac88175fc555b8
Fleuri <laur1.suomalainen@gmail.com>
2016-05-10 19:16:34 +0300

    Input tokenizing and vector creation + storage

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python
 from math import atan, cos, sin, tan
 import numpy as np
+import tokenize
 
 def main():
 
+    np.set_printoptions(threshold='nan')
+    vector_array = dict();
+
     with open("data.txt") as file:
-        print file.readlines()
+        lines = file.readlines();
+        print "Satellites: \n"
+        for line in lines:
+            words = line.split(",");
+            if (words.__len__()==4):
+              vector_array[words[0]] = llarToWorld(float(words[1]), float(words[2]), float(words[3]))
+            if (words.__len__()==5):
+                print "Routes: \n"
+                llarToWorld(float(words[1]), float(words[2]), 0)
+                llarToWorld(float(words[3]), float(words[4]), 0)
+        print vector_array.items()
 
 
-def llarToWorld(lat, lon, alt, rad=6371):
+
+def llarToWorld(lat, lon, alt, rad=6371/2):
     # see: http://www.mathworks.de/help/toolbox/aeroblks/llatoecefposition.html
     f  = 0                              # flattening
     ls = atan((1 - f)**2 * tan(lat))    # lambda
@@ -16,8 +31,8 @@ def llarToWorld(lat, lon, alt, rad=6371):
     x = rad * cos(ls) * cos(lon) + alt * cos(lat) * cos(lon)
     y = rad * cos(ls) * sin(lon) + alt * cos(lat) * sin(lon)
     z = rad * sin(ls) + alt * sin(lat)
-    vector = np.array[x,y,z]
-    print vector
+    vector = np.array([x,y,z])
+    print "vector"+ str(vector) + ","
     return vector
 
 if __name__ == "__main__":
